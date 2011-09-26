2c704e653a989a693158900d8a7091091a3b847f
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-09-26 16:32:41 -0400

    initial commit

---
+++ p2-containers-test.js
@@ -0,0 +1,148 @@
+function makeSierpinski(centerX, centerY, radius, level) {
+    if (level == 1) {
+        var result = new PolygonContainer({
+        	left: centerX - radius,
+        	top: centerY - radius,
+            centerX: centerX,
+            centerY: centerY,
+            sides: 3,
+            radius: radius,
+            borderColor: "#ffd34e",
+            fill: "#105b63",
+            borderWidth: 5,
+            polygonTheta: Math.PI / 2,
+        });
+        return result;
+    } else {
+        var result = new PolygonContainer({
+                	left: centerX - radius,
+        	top: centerY - radius,
+            centerX: centerX,
+            centerY: centerY,
+            sides: 3,
+            radius: radius,
+            borderColor: "#ffd34e",
+            borderWidth: 5,
+	    polygonTheta: Math.PI / 2,
+            fill: "#fffAD5"
+        });
+
+        var top = makeSierpinski(
+				 radius,
+				 radius / 2,
+				 radius / 2,
+				 level - 1);
+        var left = makeSierpinski(
+				  radius - Math.sqrt(3) * radius / 4,
+				  radius + radius / 4,
+				 radius / 2,
+				 level - 1);
+        var right = makeSierpinski(
+				   radius + Math.sqrt(3) * radius / 4,
+				   radius + radius / 4,
+				 radius / 2,
+				 level - 1);
+        result.children = [top, left, right];
+        return result;
+    }
+}
+
+
+
+
+
+window.onload = function () {
+
+    var canvas = document.getElementById("myCanvas");
+
+    var context = canvas.getContext("2d");
+
+    var root = new Doodle(context);
+    
+    var sier = makeSierpinski(200, 384, 200, 5);
+    
+    var rotContainer1 = new Container({ 
+        width: 150,
+		height: 150,
+		left: 900,
+		top: 300,
+		fill: "cyan",
+		borderWidth: 3,
+        theta: 3 * Math.PI / 4
+	});
+    var rotContainer2 = new Container({ 
+        width: 100,
+		height: 100,
+		left: 75,
+		top: 5,
+		fill: "purple",
+		borderWidth: 3,
+        theta: Math.PI / 4
+	});
+    var rotTextContainer = new Container({
+        left: 85,
+        top: 75,
+        theta: Math.PI
+    });
+    var rotText = new Text({ 
+        height: 40,
+        left: 5,
+        font: "32pt Helvetica",
+        fill: "white",
+        content: "UP"
+    });
+	
+	var imgFrameFrame = new Container(
+	{
+		width: 220,
+		height: 275,
+		fill:"#f5f5f5",
+		left: 350, 
+		top: 100,
+		theta: -1/6,
+		borderWidth: 2
+	}
+	);
+	var imgFrame = new Container(
+	{
+	width: 200,
+	height: 200,
+	left: 10,
+	top: 10,
+	borderWidth: 3
+	}
+	);
+	
+	imgFrame.children.push(
+		new Image({src: "kitty.jpg" })
+		);
+	imgFrameFrame.children.push(imgFrame);
+    rotContainer1.children.push(rotContainer2);
+    rotContainer2.children.push(rotTextContainer);
+    rotTextContainer.children.push(rotText);
+
+    var multiRotContainer = new Container({ 
+        width: 400,
+		height: 400,
+		left: 400,
+		top: 400,
+		borderWidth: 0
+	});
+    for(var i = 0; i < 10; i++) {
+        var newRot = new Container({ 
+            width: 100,
+		    height: 25,
+		    left: 200,
+		    top: 200,
+		    borderWidth: 4,
+            fill: "green",
+            theta: i * Math.PI / 5
+	    });
+        newRot.children.push( new Text({  left: 20, height: 17, fill: "white", content: "" + newRot.theta.toFixed(2) + " rad" }));
+        multiRotContainer.children.push(newRot);
+    }
+
+
+    root.children = [sier, rotContainer1, multiRotContainer,imgFrameFrame];
+    root.draw();
+};
---
+++ p2-primitives-test.js
@@ -0,0 +1,211 @@
+window.onload = function () {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    var doodle = new Doodle(context);
+    
+    context.save();
+
+    // Draw a frame with a straight path.
+    var framePath = new Path({
+        color: "black",
+        lineWidth: 10,
+        type: "straight",
+        points: [
+            { x: 0, y: 0 },
+            { x: 660, y: 0 },
+            { x: 660, y: 480 },
+            { x: 0, y: 480 },
+            { x: 0, y: 0 }
+        ]
+    });
+    doodle.children.push(framePath);
+    
+    // Draw the first S with a quadratic path.
+    var quadraticSbg = new Path({
+        color: "black",
+        lineWidth: 10,
+        type: "quadratic",
+        points: [
+            { x: 140, y: 100 },
+            { cp1x: 15, cp1y: 130, x: 90, y: 180 },
+            { cp1x: 215, cp1y: 230, x: 65, y: 260 }
+            ]
+    });
+    var quadraticS = new Path({
+        color: "red",
+        lineWidth: 8,
+        type: "quadratic",
+        points: [
+            { x: 140, y: 100 },
+            { cp1x: 15, cp1y: 130, x: 90, y: 180 },
+            { cp1x: 215, cp1y: 230, x: 65, y: 260 }
+            ]
+    });
+    doodle.children.push(quadraticSbg);
+    doodle.children.push(quadraticS);
+    
+    // Draw the second S with a straight path.
+    var straightSbg = new Path({
+        color: "black",
+        lineWidth: 10,
+        type: "straight",
+        points: [
+            { x: 290, y: 100 },
+            { x: 210, y: 140 },
+            { x: 290, y: 220 },
+            { x: 210, y: 270 },
+            ]
+    });
+    var straightS = new Path({
+        color: "red",
+        lineWidth: 8,
+        type: "straight",
+        points: [
+            { x: 290, y: 100 },
+            { x: 210, y: 140 },
+            { x: 290, y: 220 },
+            { x: 210, y: 270 },
+            ]
+    });
+    doodle.children.push(straightSbg);
+    doodle.children.push(straightS);
+
+    // Draw the U with a bezier path.
+    var bezierUbg = new Path({
+        color: "black",
+        lineWidth: 10,
+        type: "bezier",
+        points: [
+            { x: 340, y: 105 },
+            { cp1x: 340, cp1y: 310, cp2x: 460, cp2y: 310, x: 460, y: 110 }
+            ]
+    });
+    var bezierU = new Path({
+        color: "red",
+        lineWidth: 8,
+        type: "bezier",
+        points: [
+            { x: 340, y: 105 },
+            { cp1x: 340, cp1y: 310, cp2x: 460, cp2y: 310, x: 460, y: 110 }
+            ]
+    });
+    
+    doodle.children.push(bezierUbg);
+    doodle.children.push(bezierU);
+
+    // Draw the I with three lines.
+    var lineI1bg = new Line({
+        startX: 510,
+        startY: 115,
+        endX: 590, endY: 115,
+        lineWidth: 10,
+        color: "black"
+    });
+    var lineI2bg = new Line({
+        startX: 510,
+        startY: 275,
+        endX: 590, endY: 275,
+        lineWidth: 10,
+        color: "black"
+    });
+    var lineI3bg = new Line({
+        startX: 550,
+        startY: 115,
+        endX: 550, endY: 275,
+        lineWidth: 10,
+        color: "black"
+    });
+    var lineI1 = new Line({
+        startX: 510,
+        startY: 115,
+        endX: 590, endY: 115,
+        lineWidth: 8,
+        color: "red"
+    });
+    var lineI2 = new Line({
+        startX: 510,
+        startY: 275,
+        endX: 590, endY: 275,
+        lineWidth: 8,
+        color: "red"
+    });
+    var lineI3 = new Line({
+        startX: 550,
+        startY: 115,
+        endX: 550, endY: 275,
+        lineWidth: 8,
+        color: "red"
+    });
+    doodle.children.push(lineI1bg);
+    doodle.children.push(lineI2bg);
+    doodle.children.push(lineI3bg);
+    doodle.children.push(lineI1);
+    doodle.children.push(lineI2);
+    doodle.children.push(lineI3);
+
+    
+    // Draw Pacman and pellets with arcs and lines.
+    var arc = new Arc({
+        color: "black",
+        lineWidth: 6,
+        centerX: 180,
+        centerY: 360,
+        radius: 49,
+        startingTheta: Math.PI * 0.25,
+        endingTheta: Math.PI * 1.75,
+        counterclockwise: false
+    });
+    var pmline1 = new Line({
+        color: "black",
+        lineWidth: 4,
+        startX: 180,
+        startY: 360,
+        endX: 216.5,
+        endY: 396.5
+    });
+    var pmline2 = new Line({
+        color: "black",
+        lineWidth: 4,
+        startX: 180,
+        startY: 360,
+        endX: 216.5,
+        endY: 323.5
+    });
+    doodle.children.push(arc);
+    doodle.children.push(pmline1);
+    doodle.children.push(pmline2);
+    for (var i = 0; i < 15; i++) {
+        var arc = new Arc({
+            color: "yellow",
+            lineWidth: 6,
+            centerX: 180,
+            centerY: 360,
+            radius: 50 * (i / 15),
+            startingTheta: Math.PI * 0.25,
+            endingTheta: Math.PI * 1.75,
+            counterclockwise: false
+        });
+        doodle.children.push(arc);
+    }
+    
+    var leftValue = 250;
+    for (var i = 0; i < 3; i++) {
+        var cherry = new Image({
+            src: "cherry.jpg",
+            left: leftValue,
+            top: 310,
+            width: 100,
+            height: 100
+        });
+        doodle.children.push(cherry);
+        leftValue += 110;
+    }
+
+    // Write some text.
+    var text = new Text({ left: 360, height: 440, content: "Om nom nom" });
+
+    doodle.children.push(text);
+    
+    doodle.draw();
+};
